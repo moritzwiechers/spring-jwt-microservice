8d8bba7fef4488cebdc4ac626eacb1aa437c277b
Moritz Wiechers <wiechers@iaw.rub.de>
2023-01-03 18:26:49 +0100

    init

---
+++ src/main/java/de/gemorra/springmicroservicejwt/SpringMicroserviceJwtApplication.java
@@ -0,0 +1,13 @@
+package de.gemorra.springmicroservicejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/gemorra/springmicroservicejwt/SpringMicroserviceJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gemorra.springmicroservicejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
